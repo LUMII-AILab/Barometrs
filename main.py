c947ead9ac46ca2cc7d1f3c664f2e5570b8f9751
Daniels Buls <daniels.buls@protonmail.com>
2024-09-03 19:06:32 +0000

    Add Redis to prod build

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from fastapi.staticfiles import StaticFiles
 from routes import default
 
-app = FastAPI()
+app = FastAPI(default_response_class=ORJSONResponse)
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
