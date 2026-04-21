8c974ac8a85947ff3f5a1cf12fd144975150e891
Daniels Buls <daniels.buls@protonmail.com>
2026-04-21 21:18:31 +0200

    Remove reliance on deprecated ORJSONResponse response class.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
 from fastapi.staticfiles import StaticFiles
 from routes import default
 
-app = FastAPI(default_response_class=ORJSONResponse)
+app = FastAPI()
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
